164d1faf30eb9e02a5405512846a11b5f78f7a0b
Strikelab <contact@strikelab.fr>
2023-08-14 13:12:20 +0200

    move Router in specific component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import FicheLogement from "./pages/FicheLogement";
-import PageNotFound from "./pages/PageNotFound";
-import About from "./pages/About";
+import Router from "./components/Router";
 
 export default function App() {
-  return (
-    <div className="App">
-    <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/fiche-logment" element={<FicheLogement />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+  return <div className="App">
+    <Router />
+  </div>;
 }
---
+++ src/components/Router.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "../pages/Home";
+import FicheLogement from "../pages/FicheLogement";
+import PageNotFound from "../pages/PageNotFound";
+import About from "../pages/About";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/fiche-logment" element={<FicheLogement />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
