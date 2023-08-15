38fcdafe536da7cb4358b58feab29a006f479270
Strikelab <contact@strikelab.fr>
2023-08-15 09:52:01 +0200

    PagenotFound modifications

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="box-width">
       <Banner />
       <h1>WELCOME HOME</h1>
-      <Link to="/Un_lien_fou">Un lien qui ne mène nulle part</Link>
+      <Link to="/Un_lien_fou">🥳Un lien qui ne mène nulle part🥳</Link>
     </main>
   );
 }
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function PageNotFound() {
   return (
-    <div className="box-width">
-      <h2>404 NOT FOUND</h2>
+    <div className="box-width page-not-found ">
+      <h2>404</h2>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
+   
   );
 }
