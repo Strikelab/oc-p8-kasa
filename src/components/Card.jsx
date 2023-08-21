04f5ac22908b02c3e1b12ab49334f8b37f26b763
Strikelab <contact@strikelab.fr>
2023-08-21 21:50:13 +0200

    adding thumbnails on cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Card({ logementDatas }) {
-    const { id, title } = logementDatas
+    const { id, title, cover } = logementDatas
     return (
         <Link className="card" to={`/logement/${id}`} state={logementDatas}>
+            <img
+                className="card__thumbnail"
+                src={cover}
+                alt="aperçu du logement"
+            />
             <h2 className="card__title">{title}</h2>
         </Link>
     )
