315a559a6197131b21b377b260b38cdc78aedf78
Strikelab <contact@strikelab.fr>
2023-08-21 12:04:40 +0200

    sending datasLogement from card to ficheLogemment

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Card({ logementDatas }) {
-  const { id, title } = logementDatas;
-  return (
-    <Link  className="card" to={`/logement/${id}`}>
-      <h2 className="card__title">{title}</h2>
-    </Link>
-  );
+    const { id, title } = logementDatas
+    return (
+        <Link className="card" to={`/logement/${id}`} state={logementDatas}>
+            <h2 className="card__title">{title}</h2>
+        </Link>
+    )
 }
--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -1,5 +1,20 @@
-import React from "react";
-
+import React from 'react'
+import { useLocation } from 'react-router'
+import PageNotFound from './PageNotFound'
 export default function FicheLogement() {
-  return <div>FicheLogement</div>;
+    //we get datas from HomePage Cards Link state
+    const logementDatas = { ...useLocation().state }
+    const { id, title } = logementDatas
+    console.log(logementDatas.id)
+
+    if (id) {
+        return (
+            <div className="fiche-logement page-container">
+                <h2> Fiche Logement: {id}</h2>
+                <h3>{title}</h3>
+            </div>
+        )
+    } else {
+        return <PageNotFound />
+    }
 }
