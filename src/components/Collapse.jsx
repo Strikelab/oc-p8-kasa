02041846bda7c58936143af39dfccd08f494dea0
Strikelab <contact@strikelab.fr>
2023-08-22 14:27:35 +0200

    add some comments on Collapse.jsx

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -3,19 +3,23 @@ import { useState, useRef } from 'react'
 import arrow from '../assets/images/arrow.png'
 
 export default function Collapse({ datas, title }) {
-    //state
+    //---state
 
     const [isOpen, setIsOpen] = useState(false)
 
-    //Comportments
+    //---Comportments
+    // This toogle state value of collapsible element
     const toggle = () => setIsOpen(!isOpen)
+
+    // To obtain current collapsible element
     const parentRef = useRef()
     // if (parentRef.current) console.log(parentRef.current.scrollHeight)
-    //display
+
+    //---display
     return (
         <div className="collapse">
             <img
-                onClick={toggle}
+                onClick={toggle} // call toggle function to update state
                 className={`collapse__button ${isOpen ? 'active' : ''}`}
                 src={arrow}
                 alt="collapse button"
@@ -28,6 +32,10 @@ export default function Collapse({ datas, title }) {
                     className={`collapse__content-parent ${
                         isOpen ? 'active' : ''
                     }`}
+                    // cannot transition with the height:auto value in css
+                    // instead with the useRef() Hook we can obtain the height
+                    // of the current element and set this style value inline
+                    // to animate collapsible element
                     style={
                         isOpen
                             ? { height: parentRef.current.scrollHeight + 'px' }
