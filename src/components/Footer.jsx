5a0f4cd0b2abf58369c12153460966fe5549c1d9
Strikelab <contact@strikelab.fr>
2023-08-28 11:29:00 +0200

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Logo from "./Logo";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from './Logo'
 export default function Footer() {
-  return (
-    <footer>
-      <Link to="/">
-        <Logo />
-      </Link>
-      <p>© 2020 Kasa. All rights reserved</p>
-    </footer>
-  );
+    return (
+        <footer>
+            <Link to="/">
+                <Logo />
+            </Link>
+            <p>© 2020 Kasa. All rights&nbsp;reserved</p>
+        </footer>
+    )
 }
